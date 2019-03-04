a1771ad59c1b06f8c432f6541c6d17c4c6618659
Jaco Erasmus <jierasmus27@gmail.com>
2019-03-05 00:11:11 +0200

    Implement vuex store for state

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,72 +1,28 @@
 <template>
-    <nav class="navbar navbar-expand-lg navbar-light bg-light">
-        <router-link to="/" class="navbar-brand">StockTrader</router-link>
-        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-            <span class="navbar-toggler-icon"></span>
-        </button>
+    <nav class="navbar navbar-default">
+        <div class="container-fluid">
+            <div class="navbar-header">
+                <router-link class="navbar-brand" to="/">Brand</router-link>
+            </div>
 
-        <div class="collapse navbar-collapse" id="navbarSupportedContent">
-            <ul class="navbar-nav mr-auto">
-                <router-link to="/portfolio" class="nav-item" activeClass="active" tag="li">
-                    <a class="nav-link">Portfolio</a>
-                </router-link>
-                <router-link to="/stocks" class="nav-item" activeClass="active" tag="li">
-                    <a class="nav-link">Stocks</a>
-                </router-link>
-                <li class="nav-item">
-                    <a class="nav-link" href="#">End Day</a>
-                </li>
-                <li class="nav-item">
-                    <a class="nav-link" href="#">Save Data</a>
-                </li>
-                <li class="nav-item">
-                    <a class="nav-link" href="#">Load Data</a>
-                </li>
-            </ul>
-            <form class="form-inline my-2 my-lg-0">
-            <input class="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search">
-            <button class="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button>
-            </form>
+            <div class="collapse navbar-collapse">
+                <ul class="nav navbar-nav">
+                    <router-link to="/portfolio" tag="li" activeClass="active"><a>Portfolio</a></router-link>
+                    <router-link to="/stocks" tag="li" activeClass="active"><a>Stocks</a></router-link>
+                </ul>
+                <ul class="nav navbar-nav navbar-right">
+                    <li><a href="#">End Day</a></li>
+                    <li><a href="#">Save</a></li>
+                    <li><a href="#">Load</a></li>
+                </ul>
+            </div>
         </div>
     </nav>
-
-   <!-- <nav class="navbar navbar-expand-lg navbar-light bg-light">
-        <router-link to="/" class="navbar-brand">StockTrader</router-link>
-        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-            <span class="navbar-toggler-icon"></span>
-        </button>
-
-        <div class="collapse navbar-collapse" id="navbarSupportedContent">
-            <ul class="navbar-nav mr-auto">
-                <router-link to="/portfolio" activeClass="active" tag="li"><a>Portfolio</a></router-link>
-                <router-link to="/stocks" activeClass="active" tag="li"><a>Stocks</a></router-link>
-                <li class="nav-item">
-                    <a class="nav-link" href="#">End Day</a>
-                </li>
-                <li class="nav-item dropdown">
-                    <a 
-                        class="nav-link dropdown-toggle" 
-                        href="#" 
-                        id="navbarDropdown" 
-                        role="button" 
-                        data-toggle="dropdown" 
-                        aria-haspopup="true" 
-                        aria-expanded="false">
-                        Save and Load
-                    </a>
-                    <div class="dropdown-menu" aria-labelledby="navbarDropdown">
-                        <a class="dropdown-item" href="#">Save Data</a>
-                        <a class="dropdown-item" href="#">Load Data</a>
-                    </div>
-                </li>
-            </ul>
-        </div>
-    </nav> -->
 </template>
 
 <script>
 export default {
-    
+
 }
 </script>
 
--- src/components/stocks/Stocks.vue
+++ src/components/stocks/Stocks.vue
@@ -1,9 +1,25 @@
 <template>
-   <h1>Stocks</h1> 
+   <div>
+     <app-stock v-for="(stock, index) in stocks" :key="index" :stock="stock"></app-stock>
+   </div>
 </template>
 
 <script>
+import Stock from './Stock.vue';
+
 export default {
-    
+   components: {
+      appStock: Stock
+   },
+   computed: {
+      stocks() {
+         return this.$store.getters.stocks;
+      }
+   }
 }
 </script>
+
+<style>
+
+</style>
+
